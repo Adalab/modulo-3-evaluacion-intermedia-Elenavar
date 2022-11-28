8376af8edac15081e8633d7cfc22735756327154
Elena <elenavargasm9@gmail.com>
2022-11-28 08:48:59 +0100

    add react-starter-kit and npm-run-githubpages

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../styles/App.scss';
+// import {useEffect, useState} from 'react';
+// import callToApi from '../services/api';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
